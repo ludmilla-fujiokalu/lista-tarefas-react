78b357f63855d5b4f72ffb8b3d842d55eea5f364
Ludmilla Fujioka <fujioka.ludmilla@gmail.com>
2025-09-14 18:54:36 -0300

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,1 +1,19 @@
+import React from "react";
+
+// Componente que representa uma tarefa
+function Task({ task, toggleComplete, removeTask }) {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between", margin: "5px 0" }}>
+      <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+        {task.text}
+      </span>
+      <div>
+        <button onClick={() => toggleComplete(task.id)}>✔</button>
+        <button onClick={() => removeTask(task.id)}>❌</button>
+      </div>
+    </div>
+  );
+}
+
+export default Task;
 
